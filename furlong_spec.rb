c23cf0b203e6135b5c6c72134295260946ba7c95
knittingdev <mary@theknittingdeveloper.com>
2013-07-22 16:50:02 -0500

    Added new test and method for furlong_to_km

--- furlong_spec.rb
+++ furlong_spec.rb
@@ -6,10 +6,14 @@ describe Furlong do
 	let(:calculator) { Furlong.new }
 	
 	it "converts 1 mile to 1.60934 km" do
-		subject.miles_to_kilometers(1).should be_within(0.0001).of(1.60934)
+		calculator.miles_to_kilometers(1).should be_within(0.0001).of(1.60934)
 	end
 
 	it "converts a marathon: 26.219 miles to 42.194988 km" do
-		subject.miles_to_kilometers(26.219).should be_within(0.001).of(42.194988)
+		calculator.miles_to_kilometers(26.219).should be_within(0.001).of(42.194988)
 	end
+	
+	it "converts 1 furlong to 0.201168 km" do
+	  calculator.furlong_to_kilometers(1).should be_within(0.0001).of(0.201168)
+  end
 end
