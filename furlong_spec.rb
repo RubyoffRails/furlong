e2d8b5127266832db83fe18b58e6a213796c70d2
Annie Hsieh <ming.ting@gmail.com>
2013-08-25 18:37:43 -0500

    add furlongs to km method and tests

--- furlong_spec.rb
+++ furlong_spec.rb
@@ -12,4 +12,12 @@ describe Furlong do
 	it "converts a marathon: 26.219 miles to 42.194988 km" do
 		subject.miles_to_kilometers(26.219).should be_within(0.001).of(42.194988)
 	end
+
+	it "converts 1 furlong to 0.201168 km" do
+  	calculator.furlongs_to_km(1).should be_within(0.0001).of(0.201168)
+  end
+
+  it "converts 15 furlong to 3.01752 km" do
+    calculator.furlongs_to_km(15).should be_within(0.001).of(3.01752)
+  end
 end
