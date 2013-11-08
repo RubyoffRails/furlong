8330cc9eafbb6f2b14028b10c1f811ea2be8fc4f
Mike Capper <mikebcapp@gmail.com>
2013-11-08 21:10:42 +1300

    Not sure how to progress

--- furlong.rb
+++ furlong.rb
@@ -1,7 +1,14 @@
 class Furlong
 
+attr_reader :miles
+	def initialize(miles)
+		@miles = miles
+		
+	end
 	KM_PER_MILE = 1.60934
 	def miles_to_kilometers(miles)
 		miles * KM_PER_MILE
 	end
 end
+
+miles_to_kilometers(10)
