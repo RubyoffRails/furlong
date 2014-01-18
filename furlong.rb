c9490824d2c29f0cde82e17ee6fe5d9b4de87d67
Anthony Sexton <anthonysexton@gmail.com>
2014-01-18 11:31:54 +1100

    Added furlong to km method and created rspec and minitest tests

--- furlong.rb
+++ furlong.rb
@@ -1,7 +1,12 @@
 class Furlong
 
 	KM_PER_MILE = 1.60934
+  FURLONG_PER_KM = 0.201168
 	def miles_to_kilometers(miles)
 		miles * KM_PER_MILE
-	end
+  end
+
+  def furlong_to_kilometers(furlong)
+    furlong * FURLONG_PER_KM
+  end
 end
--- furlong_spec.rb
+++ furlong_spec.rb
@@ -4,7 +4,7 @@ require './furlong'
 describe Furlong do
 
 	let(:calculator) { Furlong.new }
-	
+
 	it "converts 1 mile to 1.60934 km" do
 		subject.miles_to_kilometers(1).should be_within(0.0001).of(1.60934)
 	end
@@ -12,4 +12,8 @@ describe Furlong do
 	it "converts a marathon: 26.219 miles to 42.194988 km" do
 		subject.miles_to_kilometers(26.219).should be_within(0.001).of(42.194988)
 	end
+
+  it "converts 1 furlong to 0.201168 kilometers" do
+    subject.furlong_to_kilometers(1).should be_within(0.000001).of(0.201168)
+  end
 end
