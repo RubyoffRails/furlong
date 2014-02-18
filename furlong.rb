b653eb95fc4b7e3e247be81c6d7762db370bfb4f
Adam Lowe <tidalinfluence@gmail.com>
2014-02-18 15:39:29 -0800

    Added a furlong to km method

--- furlong.rb
+++ furlong.rb
@@ -4,4 +4,9 @@ class Furlong
 	def miles_to_kilometers(miles)
 		miles * KM_PER_MILE
 	end
+
+  KM_PER_FURLONG = 0.201168
+  def furlongs_to_kilometers(furlongs)
+    furlongs * KM_PER_FURLONG
+  end
 end
