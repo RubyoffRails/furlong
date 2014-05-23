2384aa20ad4e8f48782ed97c4f7003fc5256e583
David Becerra <DavidBecerra84@gmail.com>
2014-05-22 20:52:54 -0700

    Add a furlong to km method

--- furlong.rb
+++ furlong.rb
@@ -1,7 +1,13 @@
 class Furlong
 
-	KM_PER_MILE = 1.60934
-	def miles_to_kilometers(miles)
-		miles * KM_PER_MILE
-	end
+  KM_PER_MILE = 1.60934
+  FURLONG_PER_KILOMETER = 0.201168
+
+  def miles_to_kilometers(miles)
+    miles * KM_PER_MILE
+  end
+
+  def furlong_to_kilometers(furlong)
+    furlong * FURLONG_PER_KILOMETER
+  end
 end
--- furlong_spec.rb
+++ furlong_spec.rb
@@ -3,13 +3,17 @@ require './furlong'
 
 describe Furlong do
 
-	let(:calculator) { Furlong.new }
-	
-	it "converts 1 mile to 1.60934 km" do
-		subject.miles_to_kilometers(1).should be_within(0.0001).of(1.60934)
-	end
+  let(:calculator) { Furlong.new }
 
-	it "converts a marathon: 26.219 miles to 42.194988 km" do
-		subject.miles_to_kilometers(26.219).should be_within(0.001).of(42.194988)
-	end
+  it "converts 1 mile to 1.60934 km" do
+    subject.miles_to_kilometers(1).should be_within(0.0001).of(1.60934)
+  end
+
+  it "converts a marathon: 26.219 miles to 42.194988 km" do
+    subject.miles_to_kilometers(26.219).should be_within(0.001).of(42.194988)
+  end
+
+  it 'converts 1 furlong to 0.201168 km' do
+    expect(subject.furlong_to_kilometers(1)).to be_within(0.001).of(0.201168)
+  end
 end
